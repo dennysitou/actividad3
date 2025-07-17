eb940f18427e9b3056fec552858d631bece75682
Ferru Lopez <lopezdelcidferrydejesus@gmail.com>
2025-07-16 22:53:51 -0600

    Peso Bucle y Verificacion

--- IMC.py
+++ IMC.py
@@ -5,6 +5,13 @@ while True:
         if 2 <= edad <= 110:
             break
     print("¡Error! Ingrese solo números entre 2 y 110")
+while True:
+    peso = input("Ingrese su peso: ")
+        peso = float(peso)
+        if 10 <= peso <= 300:
+            break
+    print("¡Error! Ingrese un peso válido")
+
 
 
 

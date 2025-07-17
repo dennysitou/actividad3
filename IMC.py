f223008f495b1031c44e7d7b5a886de6d1a81000
Dennys <dennys028ss@gmail.com>
2025-07-16 21:34:52 -0600

    Sb2

--- IMC.py
+++ IMC.py
@@ -15,36 +15,34 @@ print("Bienvenido al Sistema de Evaluación de Indice de Masa Corporal")
 
 
 #if 2 <= edad <= 19:
-    #if imc < 5:
-    #print("Bajo peso")
-    #elif 5 <= imc <= 85:
-    #print("Peso saludable")
-    #elif 85 <= imc <= 95:
-    #print("Sobrepeso")
-    #elif imc >= 95:
-    #print("Sobrepeso")
+#    if imc < 5:
+#       print("Bajo peso")
+#  elif 5 <= imc <= 85:
+#        print("Peso saludable")
+#    elif 85 < imc <= 95:
+#        print("Sobrepeso")
+#    elif imc > 95:
+#        print("Obesidad")
 
+#elif 20 <= edad <= 65:
+#    if imc < 19:
+#        print("Bajo peso")
+#    elif 19 <= imc <= 25:
+#        print("Peso saludable")
+#    elif 25 < imc <= 30:
+#        print("Sobrepeso")
+#    elif imc > 30:
+#        print("Obesidad")
 
-#if 20 <= edad <= 65:
-    #if imc < 19:
-    #print("Bajo peso")
-    #elif 19 <= imc <= 25:
-    #print("Peso saludable")
-    #elif 25 <= imc <= 30:
-    #print("Sobrepeso")
-    #elif imc >= 30:
-    #print("Sobrepeso")
-
-
-#if edad > 65:
-    #if imc < 22:
-    #print("Bajo peso")
-    #elif 22 <= imc <= 27:
-    #print("Peso saludable")
-    #elif 27 <= imc <= 30:
-    #print("Sobrepeso")
-    #elif imc >= 30:
-    #print("Sobrepeso")
+#elif edad > 65:
+#    if imc < 22:
+#        print("Bajo peso")
+#    elif 22 <= imc <= 27:
+#        print("Peso saludable")
+#    elif 27 < imc <= 30:
+#        print("Sobrepeso")
+#    elif imc > 30:
+#         print("Obesidad")
 
 
 

a5a90ce6dbe686d9a630dcf989ce0c790f5b68c8
Dennys <dennys028ss@gmail.com>
2025-07-16 20:20:11 -0600

    Primera parte del Codigo (comentado)

---
+++ IMC.py
@@ -0,0 +1,21 @@
+print("Bienvenido al Sistema de Evaluación de Indice de Masa Corporal")
+#ingresar edad(edad)
+#ingresar peso en kilogramos(peso)
+#ingresar altura en metros(altura)
+#imc = peso / altura * altura
+#print(f"Su Indice de Masa Corporal es: {IMC}")
+#EVALUACIONES DE IMC
+#if (edad < 2):
+#print("No se puede evaluar")
+
+#if 2 <= edad <= 18:
+    #if imc < 5:
+    #print("Bajo peso")
+    #elif 5 <= imc <= 85:
+    #print("Peso saludable")
+    #elif 85 <= imc <= 95:
+    #print("Sobrepeso")
+    #elif imc >= 95:
+    #print("Sobrepeso")
+
+

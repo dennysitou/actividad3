09076a00fc3f8ba0b99abc6ccf01fddfcf38aab6
Ferru Lopez <lopezdelcidferrydejesus@gmail.com>
2025-07-16 22:51:59 -0600

    Bucle y verificacion de edad

--- IMC.py
+++ IMC.py
@@ -1,49 +1,11 @@
-print("Bienvenido al Sistema de Evaluación de Indice de Masa Corporal")
-#ingresar edad(edad)
-#ingresar peso en kilogramos(peso)
-#ingresar altura en metros(altura)
-#imc = peso / altura * altura
+print("Bienvenido al Sistema de evaluación de IMC")
+while True:
+    edad = input("Ingrese su edad (2-110 años): ")
+        edad = int(edad)
+        if 2 <= edad <= 110:
+            break
+    print("¡Error! Ingrese solo números entre 2 y 110")
 
-#if (edad < 2):
-#print("No se puede evaluar")
-
-
-#print(f"Su Indice de Masa Corporal es: {imc}")
-
-
-#EVALUACIONES DE IMC
-
-
-#if 2 <= edad <= 19:
-#    if imc < 5:
-#       print("Bajo peso")
-#  elif 5 <= imc <= 85:
-#        print("Peso saludable")
-#    elif 85 < imc <= 95:
-#        print("Sobrepeso")
-#    elif imc > 95:
-#        print("Obesidad")
-
-#elif 20 <= edad <= 65:
-#    if imc < 19:
-#        print("Bajo peso")
-#    elif 19 <= imc <= 25:
-#        print("Peso saludable")
-#    elif 25 < imc <= 30:
-#        print("Sobrepeso")
-#    elif imc > 30:
-#        print("Obesidad")
-
-#elif edad > 65:
-#    if imc < 22:
-#        print("Bajo peso")
-#    elif 22 <= imc <= 27:
-#        print("Peso saludable")
-#    elif 27 < imc <= 30:
-#        print("Sobrepeso")
-#    elif imc > 30:
-#         print("Obesidad")
-#
 
 
 

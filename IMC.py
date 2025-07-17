5b1f21cd4b06c1f85c7bccd16fb39916d7e2ee33
Ferru Lopez <lopezdelcidferrydejesus@gmail.com>
2025-07-16 22:32:08 -0600

    Agregué el ingreso de variables y la formula, quité el comentario

--- IMC.py
+++ IMC.py
@@ -1,49 +1,43 @@
 print("Bienvenido al Sistema de Evaluación de Indice de Masa Corporal")
-#ingresar edad(edad)
-#ingresar peso en kilogramos(peso)
-#ingresar altura en metros(altura)
-#imc = peso / altura * altura
+edad = int(input("Ingrese su edad: "))
+peso = float(input("Ingrese su peso en kilos: "))
+altura = float(input("Ingrese su altura en metros: "))
+imc = peso / (altura * altura)
 
-#if (edad < 2):
-#print("No se puede evaluar")
+if (edad < 2):
+    print("No se puede evaluar")
+    print(f"Su Indice de Masa Corporal es: {imc}")
 
+if 2 <= edad <= 19:
+    if imc < 5:
+       print("Bajo peso")
+    elif 5 <= imc <= 85:
+        print("Peso saludable")
+    elif 85 < imc <= 95:
+        print("Sobrepeso")
+    elif imc > 95:
+        print("Obesidad")
 
-#print(f"Su Indice de Masa Corporal es: {imc}")
+elif 20 <= edad <= 65:
+    if imc < 19:
+        print("Bajo peso")
+    elif 19 <= imc <= 25:
+        print("Peso saludable")
+    elif 25 < imc <= 30:
+        print("Sobrepeso")
+    elif imc > 30:
+        print("Obesidad")
 
+elif edad > 65:
+    if imc < 22:
+        print("Bajo peso")
+    elif 22 <= imc <= 27:
+        print("Peso saludable")
+    elif 27 < imc <= 30:
+        print("Sobrepeso")
+    elif imc > 30:
+         print("Obesidad")
 
-#EVALUACIONES DE IMC
-
-
-#if 2 <= edad <= 19:
-#    if imc < 5:
-#       print("Bajo peso")
-#  elif 5 <= imc <= 85:
-#        print("Peso saludable")
-#    elif 85 < imc <= 95:
-#        print("Sobrepeso")
-#    elif imc > 95:
-#        print("Obesidad")
-
-#elif 20 <= edad <= 65:
-#    if imc < 19:
-#        print("Bajo peso")
-#    elif 19 <= imc <= 25:
-#        print("Peso saludable")
-#    elif 25 < imc <= 30:
-#        print("Sobrepeso")
-#    elif imc > 30:
-#        print("Obesidad")
-
-#elif edad > 65:
-#    if imc < 22:
-#        print("Bajo peso")
-#    elif 22 <= imc <= 27:
-#        print("Peso saludable")
-#    elif 27 < imc <= 30:
-#        print("Sobrepeso")
-#    elif imc > 30:
-#         print("Obesidad")
-#
 
 
 
